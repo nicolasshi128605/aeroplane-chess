e366638d724d58ac95ef7a8cedeb8d820ce9fa39
YongZhang980121 <zhang980121@gmail.com>
2024-07-14 18:18:21 +0800

    update

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,20 +1,17 @@
-using System;
 using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    private void Awake()
+    public void ChangeColor(int i)
     {
-        Debug.Log("Awake");
-    }
-
-    private void Start()
-    {
-        Debug.Log("start");
-    }
-
-    private void Update()
-    {
-        Debug.Log("update");
+        var spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        spriteRenderer.color = (i % 4) switch
+        {
+            0 => new Color(1f, 0f, 0f, 0.5f),
+            1 => Color.blue,
+            2 => Color.green,
+            3 => Color.yellow,
+            _ => spriteRenderer.color
+        };
     }
 }
