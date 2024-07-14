85155fec74dd00d57ddccb19a13c18f44e91a726
YongZhang980121 <zhang980121@gmail.com>
2024-07-14 18:21:43 +0800

    random

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,13 +5,23 @@ public class Tile : MonoBehaviour
     public void ChangeColor(int i)
     {
         var spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-        spriteRenderer.color = (i % 4) switch
+        // spriteRenderer.color = (i % 4) switch
+        // {
+        //     0 => new Color(1f, 0f, 0f, 0.5f),
+        //     1 => Color.blue,
+        //     2 => Color.green,
+        //     3 => Color.yellow,
+        //     _ => spriteRenderer.color
+        // };
+        var randomValue = Random.Range(0, 4);
+
+        spriteRenderer.color = (randomValue switch
         {
             0 => new Color(1f, 0f, 0f, 0.5f),
             1 => Color.blue,
             2 => Color.green,
             3 => Color.yellow,
             _ => spriteRenderer.color
-        };
+        });
     }
 }
