bb8f12a3623dc198342066b70514fe59a96e4cce
nicolasshi128605 <nicolaszhanchenshi@gmail.com>
2024-08-06 18:28:22 -0400

    1

--- Assets/Player_move.cs
+++ Assets/Player_move.cs
@@ -7,7 +7,7 @@ public class Player_move : MonoBehaviour
 {
     private Dice dice;
     public int CurrentLocation = 0;
-
+    public List<Sprite> pointImage;
     public Vector3[] moveLocations;
 
     private void Awake()
@@ -15,7 +15,12 @@ public class Player_move : MonoBehaviour
         //todo 添加新增的格子坐标，并摆好格子位置
         moveLocations = new Vector3[]
         {
+           
+           
+           
+           
             // First set of tiles (j == 0)
+            new Vector3(115f, 115f, 0f), // Top-right corner
             new Vector3(75f, 115f, 0f),
             new Vector3(45f, 115f, 0f),
             new Vector3(15f, 115f, 0f),
@@ -24,6 +29,7 @@ public class Player_move : MonoBehaviour
             new Vector3(-75f, 115f, 0f),
 
             // Second set of tiles (j == 1)
+            new Vector3(-115f, 115f, 0f), // Top-left corner
             new Vector3(-115f, 75f, 0f),
             new Vector3(-115f, 45f, 0f),
             new Vector3(-115f, 15f, 0f),
@@ -33,20 +39,22 @@ public class Player_move : MonoBehaviour
 
             // First set of tiles (j == 1)
             //todo 这一组反了
-            new Vector3(75f, -115f, 0f),
-            new Vector3(45f, -115f, 0f),
-            new Vector3(15f, -115f, 0f),
-            new Vector3(-15f, -115f, 0f),
-            new Vector3(-45f, -115f, 0f),
+            new Vector3(-115f, -115f, 0f), // Bottom-left corner
             new Vector3(-75f, -115f, 0f),
+            new Vector3(-45f, -115f, 0f),
+            new Vector3(-15f, -115f, 0f),
+            new Vector3(15f, -115f, 0f),
+            new Vector3(45f, -115f, 0f),
+            new Vector3(75f, -115f, 0f),
 
             // Second set of tiles (j == 0)
-            new Vector3(115f, 75f, 0f),
-            new Vector3(115f, 45f, 0f),
-            new Vector3(115f, 15f, 0f),
-            new Vector3(115f, -15f, 0f),
-            new Vector3(115f, -45f, 0f),
+            new Vector3(115f, -115f, 0f), // Bottom-right corner
             new Vector3(115f, -75f, 0f),
+            new Vector3(115f, -45f, 0f),
+            new Vector3(115f, -15f, 0f),
+            new Vector3(115f, 15f, 0f),
+            new Vector3(115f, 45f, 0f),
+            new Vector3(115f, 75f, 0f),
         };
     }
 
@@ -83,6 +91,19 @@ public class Player_move : MonoBehaviour
         {
             CurrentLocation = (CurrentLocation + 1) % moveLocations.Length;
             transform.DOLocalMove(moveLocations[CurrentLocation], 1f).SetEase(Ease.Linear);
+            if (CurrentLocation==0)
+            {
+                spriteRenderer.sprite = pointImage[2f];
+            }else if(CurrentLocation==7)
+            {
+                spriteRenderer.sprite = pointImage[0f];
+            }else if(CurrentLocation==14)
+            {
+                spriteRenderer.sprite = pointImage[3f];
+            }else if(CurrentLocation==21)
+            {
+                spriteRenderer.sprite = pointImage[1f];
+            }
         }
 
         Debug.Log("Player moved to location: " + transform.position);
