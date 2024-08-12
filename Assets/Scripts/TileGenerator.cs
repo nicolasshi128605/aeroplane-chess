c9b6d4e66e88cc201e4e840caf914fb6edc7bd12
nicolasshi128605 <nicolaszhanchenshi@gmail.com>
2024-08-12 09:30:35 -0400

    1

--- Assets/Scripts/TileGenerator.cs
+++ Assets/Scripts/TileGenerator.cs
@@ -32,5 +32,12 @@ public class TileGenerator : MonoBehaviour
                     Quaternion.identity, transform);
             }
         }
+        
+        
+        
+        
+        
+        
+        
     }
 }
