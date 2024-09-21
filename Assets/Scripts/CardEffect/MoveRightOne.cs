0572a13836d0954cdc02e9cba95dc575e1704567
YongZhang980121 <zhang980121@gmail.com>
2024-09-22 04:33:44 +0800

    card
    card

--- Assets/Scripts/CardEffect/MoveRightOne.cs
+++ Assets/Scripts/CardEffect/MoveRightOne.cs
@@ -1,4 +1,5 @@
-using UnityEngine;
+using DG.Tweening;
+using Managers;
 
 namespace CardEffect
 {
@@ -6,8 +7,22 @@ namespace CardEffect
     {
         public override void ApplyCardEffect()
         {
-            Debug.Log("MoveRightOne");
-            base.ApplyCardEffect();
+            var currentTile = Global.Player.currentTIle;
+            Global.Player.Jump(0.5f);
+            if (currentTile.rightTile != null)
+            {
+                Global.Player.ChangeToRight();
+                Global.Player.transform.DOMove(currentTile.rightTile.transform.position, 0.5f).SetEase(Ease.Linear)
+                    .OnComplete(() =>
+                    {
+                        currentTile.rightTile.SetPlayerHere(Global.Player);
+                        base.ApplyCardEffect();
+                    });
+            }
+            else
+            {
+                DOVirtual.DelayedCall(0.5f, () => { base.ApplyCardEffect(); });
+            }
         }
     }
 }
