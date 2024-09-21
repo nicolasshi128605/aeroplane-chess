81406a8bbf025d5fac5ebe5300cb9230a3330f6f
YongZhang980121 <zhang980121@gmail.com>
2024-09-22 00:16:21 +0800

    jumpshadow
    jumpshadow

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using Enums;
 using Managers;
 using Tile;
@@ -8,6 +9,7 @@ namespace Player
     public class Player : MonoBehaviour
     {
         public SpriteRenderer image;
+        public Transform shadow;
         public Sprite upImage;
         public Sprite downImage;
         public Sprite leftImage;
@@ -37,6 +39,18 @@ namespace Player
                 hpMax = 8;
                 hp = 8;
             }
+        }
+
+        public void Jump(float duration)
+        {
+            image.transform.DOLocalMoveY(1.6f, duration / 2f).From(0.6f).SetEase(Ease.OutSine).OnComplete(() =>
+            {
+                image.transform.DOLocalMoveY(0.6f, duration / 2f).From(1.6f).SetEase(Ease.InSine);
+            });
+            shadow.DOScale(0.8f, duration / 2f).From(1f).SetEase(Ease.OutSine).OnComplete(() =>
+            {
+                shadow.DOScale(1f, duration / 2f).From(0.8f).SetEase(Ease.InSine);
+            });
         }
 
         public void PlayerMove(int moveStep)
